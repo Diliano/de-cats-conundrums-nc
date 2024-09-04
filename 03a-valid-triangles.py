537f2eea1cbb6090d5d483a8004cd6f1621eff68
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-04 11:39:48 +0100

    add the solution for 03a-valid-triangles

--- 03a-valid-triangles.py
+++ 03a-valid-triangles.py
@@ -7,10 +7,21 @@ from test_api.checks import run_test, skip_test, format_err_msg
 # To be a valid triangle, the sum of any two sides must be larger than the
 #  remaining side
 
+# permutations:
+# side1 + side2 must be > side3
+# side1 + side3 must be > side 2
+# side2 + side3 must be > side 1
 
 def valid_triangles(triangles):
-    pass
+    count = 0
 
+    for triangle in triangles:
+        side1, side2, side3 = triangle[0], triangle[1], triangle[2]
+        if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1):
+            count += 1
+
+    return count
+    
 
 @run_test
 def test_valid_triangles_returns_0_when_passed_no_triangles():
@@ -18,19 +29,19 @@ def test_valid_triangles_returns_0_when_passed_no_triangles():
         format_err_msg(0, valid_triangles([]))
 
 
-@skip_test
+@run_test
 def test_valid_triangles_returns_0_when_passed_a_list_with_no_valid_triangles():
     assert valid_triangles([[5, 10, 25]]) == 0, \
         format_err_msg(0, valid_triangles([[5, 10, 25]]))
 
 
-@skip_test
+@run_test
 def test_valid_triangles_returns_1_when_passed_a_list_with_a_single_valid_triangle():
     assert valid_triangles([[5, 4, 5]]) == 1, \
         format_err_msg(1, valid_triangles([[5, 4, 5]]))
 
 
-@skip_test
+@run_test
 def test_valid_triangles_returns_2_when_passed_a_list_with_2_valid_and_1_invalid_triangle():
     assert valid_triangles([[5, 10, 25], [5, 4, 5], [542, 586, 419]]) == 2, \
         format_err_msg(2, valid_triangles(
